baf3bf451d1b8dc384f8aebbd1fc535d52c658d7
Julio Cesar <145167974+Juli-oCosta@users.noreply.github.com>
2025-09-17 11:58:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 logo = """
- ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
-a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8  
-8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88          
-"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
- '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88   
-            88             88                                 
-           ""             88                                 
-                          88                                 
- ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
-a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
-8b         88 88       d8 88       88 8PP""""""" 88          
-"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
- '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88          
-              88                                             
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,
+a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8
+8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88
+ '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88
+            88             88
+           ""             88
+                          88
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
+8b         88 88       d8 88       88 8PP""""""" 88
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88
+ '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88
+              88
               88
 """
 
@@ -35,18 +35,31 @@ def caesarCypher(original_text, shift_amount, encode_or_decode):
         new_letter = alphabet[new_position]
 
         rearranged_text.append(new_letter)
-    
+
     print(f"Here's the {encode_or_decode}d result:", *rearranged_text)
 
 over = False
 while not over:
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction == "encode" or direction == "decode":
+      break
+    else:
+      print("Invalid option. Please, type only 'encode' or 'decode'.")
+
   text = input("Type in your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
 
   caesarCypher(original_text=text, shift_amount = shift, encode_or_decode=direction)
 
-  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
-  if restart == "no":
-    over = True
-    print("Goodbye!")
+  while True:
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if restart == "no":
+      over = True
+      print("Goodbye!")
+      break
+    elif restart == "yes":
+      over = False
+      break
+    else:
+      print("Invalid option. Please, type only 'yes' or 'no'.")
